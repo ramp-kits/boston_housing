ce274f1aa3f7f09062bf4c2c4ecff6fa031f658d
Balazs Kegl <balazs.kegl@gmail.com>
2017-06-03 19:16:02 +0200

    backend

--- backend.py
+++ backend.py
@@ -1,14 +1,25 @@
 import os
+import imp
 import pandas as pd
 from sklearn.model_selection import ShuffleSplit
-from .problem import (  # noqa
-#    get_cv,
-    problem_title,
-    prediction_type,
-    workflow,
-    prediction_labels,
-    target_column_name,
-)
+
+problem = imp.load_source('', 'problem.py')
+problem_title = problem.problem_title
+prediction_type = problem.prediction_type
+prediction_labels = problem.prediction_labels
+workflow = problem.workflow
+target_column_name = problem.target_column_name
+# You can use the starting kit CV or redefine the get_cv function
+# get_cv = problem.get_cv
+
+
+def get_cv(X, y):
+    cv = ShuffleSplit(n_splits=8, test_size=0.2, random_state=57)
+    return cv.split(X)
+
+
+def prepare_data():
+    pass
 
 
 def get_train_data(path='.'):
@@ -23,8 +34,3 @@ def get_test_data(path='.'):
     y_array = data[target_column_name].values
     X_array = data.drop([target_column_name], axis=1).values
     return X_array, y_array
-
-
-def get_cv(X, y):
-    cv = ShuffleSplit(n_splits=8, test_size=0.2, random_state=57)
-    return cv.split(X)
