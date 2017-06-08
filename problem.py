167c0e56eaa95b5fef70257a223ace2c6d35420c
Balazs Kegl <balazs.kegl@gmail.com>
2017-06-08 13:35:42 +0200

    backend defined in test

--- problem.py
+++ problem.py
@@ -19,27 +19,27 @@ def get_cv(X, y):
     cv = ShuffleSplit(n_splits=8, test_size=0.2, random_state=57)
     return cv.split(X)
 
-target_column_name = 'medv'
-# change this when you test the backend
-is_backend = False
+_target_column_name = 'medv'
 
 
 def _read_data(path, f_name):
     data = pd.read_csv(os.path.join(path, 'data', f_name))
-    y_array = data[target_column_name].values
-    X_array = data.drop([target_column_name], axis=1).values
+    y_array = data[_target_column_name].values
+    X_array = data.drop([_target_column_name], axis=1).values
     return X_array, y_array
 
 
 def get_train_data(path='.'):
     f_name = 'public_train.csv'
-    if is_backend:
+    # is_backend is defined in rampwf.test_submission
+    if is_backend:  # noqa
         f_name = 'train.csv'
     return _read_data(path, f_name)
 
 
 def get_test_data(path='.'):
     f_name = 'public_test.csv'
-    if is_backend:
+    # is_backend is defined in rampwf.test_submission
+    if is_backend:  # noqa
         f_name = 'test.csv'
     return _read_data(path, f_name)
