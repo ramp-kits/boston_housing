6f0ac1726f363671ad5b6e006ec20c440e2e517d
Lucy Liu <jliu176@gmail.com>
2020-04-17 15:38:28 +0200

    change to reg

--- submissions/random_forest_50/estimator.py
+++ submissions/random_forest_50/estimator.py
@@ -2,6 +2,6 @@ from sklearn.ensemble import RandomForestRegressor
 
 
 def get_estimator():
-    pipeline = RandomForestRegressor(
+    reg = RandomForestRegressor(
         n_estimators=50, max_leaf_nodes=3, random_state=61)
-    return pipeline
+    return reg
--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -2,6 +2,6 @@ from sklearn.ensemble import RandomForestRegressor
 
 
 def get_estimator():
-    pipeline = RandomForestRegressor(
+    reg = RandomForestRegressor(
         n_estimators=2, max_leaf_nodes=2, random_state=61)
-    return pipeline
+    return reg
